67c4052f15d6720b095e2f013c6f583f4ba157ed
Joakim Sandström <joakim.s@webbkontakt.net>
2019-09-29 11:07:40 +0200

    Setting utf and letting ncurses draw lines
    User addition to get lines properly

--- netflix_prep_install.sh
+++ netflix_prep_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Set UTF-8; e.g. “en_US.UTF-8” or “de_DE.UTF-8”:
+export LC_ALL=“C.UTF-8”
+
+#Tell ncurses to use line characters that work with UTF-8.
+export NCURSES_NO_UTF8_ACS=1
+
 cd /home/osmc
 sudo apt-get update 2>&1 | dialog --title "Updating package database and installing needed .deb packages..." --infobox "\nPlease wait...\n" 11 70 
 if [ ! -d /usr/share/doc/python-crypto ]; then
