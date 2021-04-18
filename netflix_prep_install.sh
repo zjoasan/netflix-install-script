d29d640a556e4f28d5c4fdfd04d3370ca3244adc
Joakim Sandström <joakim.s@webbkontakt.net>
2021-04-18 05:04:32 +0200

    tried a kodi version sensing algorithm
    Tried to use the same script for both v.18 and v.19 of Kodi, Right now, while OSMC is testing v.19, there seems to be a similar issue, as in Leia nightly, which is no automated dependencies installation. So I chose to do a scripted install of current addon dependencies, for the Netflix addon.

--- netflix_prep_install.sh
+++ netflix_prep_install.sh
@@ -9,42 +9,69 @@ sudo locale-gen en_US.UTF-8
 export NCURSES_NO_UTF8_ACS=1
 
 cd /home/osmc
-sudo apt-get update 2>&1 | dialog --title "Updating package database and installing needed .deb packages..." --infobox "\nPlease wait...\n" 11 70 
-if [ ! -d /usr/share/doc/python-crypto ]; then
-	sudo apt-get install -q -y python-crypto
-fi
-sudo apt-get install -q -y build-essential python-pip libnss3 libnspr4
+# Map parameters to coder-friendly names.
+Program="kodi"
+Version="19"
 
-dialog --title "Installing python dependencies..." --infobox "\nPlease wait...\n" 11 70
-sudo pip install -q -U setuptools
-sudo pip install -q wheel
-sudo pip install -q pycryptodomex
+# Program name must be a valid command.
+command -v $Program >/dev/null 2>&1 || { echo "Command: $Program not found."; exit 99; }
+InstalledVersion=$( "$Program" --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/' )
+sudo apt-get update 2>&1 | dialog --title "Updating package database and installing needed .deb packages..." --infobox "\nPlease wait...\n" 11 70 
+if [[ $InstalledVersion -lt $version ]]; then
+   if [ ! -d /usr/share/doc/python-crypto ]; then
+	sudo apt-get install -q -y python-crypto
+   fi
+   sudo apt-get install -q -y build-essential python-pip libnss3 libnspr4
+
+   dialog --title "Installing python dependencies..." --infobox "\nPlease wait...\n" 11 70
+   sudo pip install -q -U setuptools
+   sudo pip install -q wheel
+   sudo pip install -q pycryptodomex
+else 
+   dialog --title "Installing python3 dependencies..." --infobox "\nPlease wait...\n" 11 70
+
+   sudo apt-get install python3-crypto
+   sudo apt-get install build-essential python3-pip
+   sudo python3 -m pip install -U setuptools
+   sudo python3 -m pip install wheel
+   sudo python3 -m pip install pycryptodomex
+fi   
 
 if [ ! -d "/home/osmc/addons" ]; then
 	mkdir addons
 fi
 cd addons
+
 dialog --title "Downloading Netflix repository" --infobox "\nPlease wait...\n" 11 70
 
-## Just keeping two versions of the add-on
+## Just keeping the last two downloads of the repository
 if [ -f "./netflix-repo.zip" ]; then
 	if [ -f "./netflix-repo.zip.old" ]; then
 		rm netflix-repo.zip.old
 	fi
 	mv netflix-repo.zip netflix-repo.zip.old
 fi
-wget -q -O netflix-repo.zip https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.1.zip
-#for Matrix use this wget 
-# wget -q -O netflix-repo.zip https://github.com/castagnait/repository.castagnait/raw/matrix/repository.castagnait-1.0.0.zip
 
-#from a rasbian problem solution for extracting the widevine libary, dont know if it's a OSMC issue though, don't se a real security issue, but you do as you please with this line, worked before without it in OSMC
-if [ ! $(sudo cat /etc/sudoers | grep 'osmc ALL=NOPASSWD:/bin/mount,' | wc -l) ]; then
-	sudo sh -c  "echo 'osmc ALL=NOPASSWD:/bin/mount,/bin/umount,/sbin/losetup,/sbin/modprobe' >> /etc/sudoers"
+if [[ $InstalledVersion -lt $version ]]; then
+   wget -q -O netflix-repo.zip https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.1.zip
+else
+   wget -q -O netflix-repo.zip https://github.com/castagnait/repository.castagnait/raw/matrix/repository.castagnait-1.0.0.zip
 fi
 
+#fix for non automatic dependcies install, had the same issue with nightly leia
+if [[ $InstalledVersion -ge $version ]]; then
+   kodi-send --action="InstallAddon(script.module.addon.signals)"
+   kodi-send --action="InstallAddon(script.module.certifi)"
+   kodi-send --action="InstallAddon(script.module.chardet)"
+   kodi-send --action="InstallAddon(script.module.idna)"
+   kodi-send --action="InstallAddon(script.module.urllib3)"
+   kodi-send --action="InstallAddon(script.module.requests)"
+   kodi-send --action="InstallAddon(script.module.myconnpy)"
+fi   
+ 
 sudo systemctl stop mediacenter
 sleep 5
-dialog --title "Installation finished!" --msgbox "\nThank you for using my installer\nNow go to addon-browser and choose install from zip\nNavigate to homefolder/addons and install Netflix plugin." 11 70
+dialog --title "Installation finished!" --msgbox "\nThank you for using my installer\nNow go to addon-browser and choose install from zip\nNavigate to homefolder/addons and install Netflix repository." 11 70
 if [ -f "/home/osmc/netflix_prep_install.sh" ]; then
 	rm /home/osmc/netflix_prep_install.sh
 fi	
