1c622c35d49cbd74eaeccc545264f0132db5a600
Joakim Sandström <joakim.s@webbkontakt.net>
2021-08-24 22:40:41 +0200

    Fixes
    Fault tolerance cd, removed CR, fixed variable name typeO and remarked addon-deps installs

--- netflix_prep_install.sh
+++ netflix_prep_install.sh
@@ -8,10 +8,11 @@ sudo locale-gen en_US.UTF-8
 #Tell ncurses to use line characters that work with UTF-8.
 export NCURSES_NO_UTF8_ACS=1
 
-cd /home/osmc
+cd /home/osmc || exit
 # Map parameters to coder-friendly names.
 Program="kodi"
-Version="19"
+version="19"
+# thanks DavorPerkovac for TypeO in above varible namne
 
 # Program name must be a valid command.
 command -v $Program >/dev/null 2>&1 || { echo "Command: $Program not found."; exit 99; }
@@ -28,7 +29,7 @@ if [[ $NonDecIV -lt $version ]]; then
    sudo pip install -q -U setuptools
    sudo pip install -q wheel
    sudo pip install -q pycryptodomex
-else 
+else
    dialog --title "Installing python3 dependencies..." --infobox "\nPlease wait...\n" 11 70
 
    sudo apt-get install python3-crypto
@@ -36,12 +37,12 @@ else
    sudo python3 -m pip install -U setuptools
    sudo python3 -m pip install wheel
    sudo python3 -m pip install pycryptodomex
-fi   
+fi
 
 if [ ! -d "/home/osmc/addons" ]; then
 	mkdir addons
 fi
-cd addons
+cd addons || exit
 
 dialog --title "Downloading Netflix repository" --infobox "\nPlease wait...\n" 11 70
 
@@ -60,23 +61,25 @@ else
 fi
 
 #fix for non automatic dependcies install, had the same issue with nightly leia
-if [[ $NonDecIV -ge $version ]]; then
-   kodi-send --action="InstallAddon(script.module.addon.signals)"
-   kodi-send --action="InstallAddon(script.module.certifi)"
-   kodi-send --action="InstallAddon(script.module.chardet)"
-   kodi-send --action="InstallAddon(script.module.idna)"
-   kodi-send --action="InstallAddon(script.module.urllib3)"
-   kodi-send --action="InstallAddon(script.module.requests)"
-   dialog --title "Kodi interaction!" --msgbox "\nInstalling some addons needed\nThey need user confirmation, so look at your Kodi screen." 11 70
-   kodi-send --action="InstallAddon(script.module.myconnpy)"
-   kodi-send --action="InstallAddon(script.module.inputstreamhelper)"
-fi   
- 
+# if [[ $NonDecIV -ge $version ]]; then
+#    kodi-send --action="InstallAddon(script.module.addon.signals)"
+#    kodi-send --action="InstallAddon(script.module.certifi)"
+#    kodi-send --action="InstallAddon(script.module.chardet)"
+#    kodi-send --action="InstallAddon(script.module.idna)"
+#    kodi-send --action="InstallAddon(script.module.urllib3)"
+#    kodi-send --action="InstallAddon(script.module.requests)"
+#    dialog --title "Kodi interaction!" --msgbox "\nInstalling some addons needed\nThey need user confirmation, so look at your Kodi screen." 11 70
+#    kodi-send --action="InstallAddon(script.module.myconnpy)"
+#    kodi-send --action="InstallAddon(script.module.inputstreamhelper)"
+# fi
+
 sudo systemctl stop mediacenter
 sleep 5
 dialog --title "Installation finished!" --msgbox "\nThank you for using my installer\nNow go to addon-browser and choose install from zip\nNavigate to homefolder/addons and install Netflix repository." 11 70
 if [ -f "/home/osmc/netflix_prep_install.sh" ]; then
 	rm /home/osmc/netflix_prep_install.sh
-fi	
+fi
 sudo systemctl start mediacenter
+
+
 
